7f5eb0356408104eefe9e52074dc69b94f123aca
MaryBhavana <bhavanagollapalli123@gmail.com>
2022-07-05 16:39:52 +0530

    myapp

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,6 +1,6 @@
 import {createStore} from 'redux';
 import Reducer from './Reducer';
 
-const store =createStore(Reducer);
+const Store =createStore(Reducer);
 
-export default store;
+export default Store;
